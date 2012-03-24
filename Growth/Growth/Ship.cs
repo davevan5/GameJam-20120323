9e0037876ce017c00adf291fb28e4d261b1f0d7e
ginja85 <davidevans1985@gmail.com>
2012-03-24 09:52:26 +0000

    Added ship class

---
+++ Growth/Growth/Ship.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Growth
+{
+    public class Ship
+    {
+        public Texture2D Texture;
+        public Vector2 Position;
+        public Vector2 Velocity;
+        public float Rotation;
+
+        public int Health;
+
+        public void Update(double dt)
+        {
+            Position = Velocity * (float)dt;
+        }
+    }
+}
